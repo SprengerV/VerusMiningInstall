106f6f66f4866475e579b992bdc443b4eba839ae
Brendan Francis <B.Francis@SprengerV.com>
2025-01-10 08:24:02 -0500

    fixed threads, double quotes, echo -e

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 announce() {
-    echo '\n--------------------------------\n $1 \n--------------------------------\n' &&
+    echo -e "\n--------------------------------\n $1 \n--------------------------------\n"
 }
 
 announce 'Updating system...            ' &&
@@ -25,9 +25,9 @@ announce 'Running build script...       ' &&
 announce 'Creating config directory...  ' &&
 mkdir ~/.ccminer &&
 announce 'Creating config file...       ' &&
-echo -e '{\n\t"_note": "Custom Configuration: SprengerV",\n\n\t"pools":[{\n\t\t"name": "Verus Community Pool",\n\t\t"url": "stratum+tcp://pool.verus.io:9999",\n\t\t"user": "RXbFFysmgJD5npM8HJVnqaXsJ8xBde7QcG.NewPhone",\n\t\t"pass": "x"\n\t},\n\t{\n\t\t"name": "Luck Pool",\n\t\t"url": "stratum+tcp://na.luckpool.net:3957",\n\t\t"user": "RXbFFysmgJD5npM8HJVnqaXsJ8xBde7QcG.NewPhone",\n\t\t"pass": "x"\n\t}],\n\n\t"algo": "verus",\n\t"threads": $(( $(nproc) - 2 )),\n\n\t"timeout": 60,\n\n\t"api-bind": "0.0.0.0",\n\t"api-remote": true,\n\n\t"no-gbt": true\n}' > ~/.ccminer/ccminer.conf &&
+echo -e '{\n\t"_note": "Custom Configuration: SprengerV",\n\n\t"pools":[{\n\t\t"name": "Verus Community Pool",\n\t\t"url": "stratum+tcp://pool.verus.io:9999",\n\t\t"user": "RXbFFysmgJD5npM8HJVnqaXsJ8xBde7QcG.NewPhone",\n\t\t"pass": "x"\n\t},\n\t{\n\t\t"name": "Luck Pool",\n\t\t"url": "stratum+tcp://na.luckpool.net:3957",\n\t\t"user": "RXbFFysmgJD5npM8HJVnqaXsJ8xBde7QcG.NewPhone",\n\t\t"pass": "x"\n\t}],\n\n\t"algo": "verus",\n\t"threads": '$(( $(nproc) - 2 ))',\n\n\t"timeout": 60,\n\n\t"api-bind": "0.0.0.0",\n\t"api-remote": true,\n\n\t"no-gbt": true\n}' > ~/.ccminer/ccminer.conf &&
 announce 'Creating mining script...     ' &&
-echo -e '#!/bin/bash\n\n$(pwd)/ccminer -c ~/.ccminer/ccminer.conf' > mine.sh &&
+echo -e '#!/bin/bash\n\n/usr/src/ccminer/ccminer -c ~/.ccminer/ccminer.conf' > mine.sh &&
 announce 'Changing script permissions...' &&
 sudo chmod +x mine.sh &&
 announce 'Installing script...          ' &&
