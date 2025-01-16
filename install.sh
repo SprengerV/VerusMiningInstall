a07b73708821f77aa099386911b178b7032ae023
Brendan Francis <B.Francis@SprengerV.com>
2025-01-16 18:51:27 -0500

    removed background from gpu config file

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ announce   'Creating config files...      '                           &&
 echo       -e '{\n\t"_note": "Custom Configuration: SprengerV",\n\n\t"pools":[{\n\t\t"name": "Verus Community Pool",\n\t\t"url": "stratum+tcp://pool.verus.io:9999",\n\t\t"user": "'$WALLET'.'$WORKER'",\n\t\t"pass": "x"\n\t},\n\t{\n\t\t"name": "Luck Pool",\n\t\t"url": "stratum+tcp://na.luckpool.net:3957",\n\t\t"user": "'$WALLET'.'$WORKER'",\n\t\t"pass": "x"\n\t}],\n\n\t"algo": "verus",\n\t"threads": '$THREADS',\n\n\t"timeout": 60,\n\n\t"api-bind": "0.0.0.0",\n\t"api-remote": true,\n\n\t"no-gbt": true\n}'                   \
     > ~/.ccminer/ccminer-cpu.conf                                     &&
 cp         ~/.ccminer/ccminer-cpu.conf ~/.ccminer/ccminer-gpu.conf    &&
-sed        -i 's/"threads": [0-9]*,/"background": true,/g' ~/.ccminer/ccminer-gpu.conf
+sed        -i 's/"threads": [0-9]*,//g' ~/.ccminer/ccminer-gpu.conf
 announce   'Creating mining scripts...    '                           &&
 echo       -e '#!/bin/bash\n\n/usr/src/ccminer/ccminer -c ~/.ccminer/ccminer-cpu.conf' > mine-cpu.sh                                   &&
 echo       -e '#!/bin/bash\n\n/usr/src/ccminer/ccminer -c ~/.ccminer/ccminer-gpu.conf' > mine-gpu.sh                                   &&
