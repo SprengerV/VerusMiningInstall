eb8db3f83a5c9f1941708e5a3cce05e5e43e8b2f
Brendan Francis <B.Francis@SprengerV.com>
2025-01-16 19:02:38 -0500

    added screen script

--- install.sh
+++ install.sh
@@ -34,16 +34,19 @@ announce   'Creating config files...      '                           &&
 echo       -e '{\n\t"_note": "Custom Configuration: SprengerV",\n\n\t"pools":[{\n\t\t"name": "Verus Community Pool",\n\t\t"url": "stratum+tcp://pool.verus.io:9999",\n\t\t"user": "'$WALLET'.'$WORKER'",\n\t\t"pass": "x"\n\t},\n\t{\n\t\t"name": "Luck Pool",\n\t\t"url": "stratum+tcp://na.luckpool.net:3957",\n\t\t"user": "'$WALLET'.'$WORKER'",\n\t\t"pass": "x"\n\t}],\n\n\t"algo": "verus",\n\t"threads": '$THREADS',\n\n\t"timeout": 60,\n\n\t"api-bind": "0.0.0.0",\n\t"api-remote": true,\n\n\t"no-gbt": true\n}'                   \
     > ~/.ccminer/ccminer-cpu.conf                                     &&
 cp         ~/.ccminer/ccminer-cpu.conf ~/.ccminer/ccminer-gpu.conf    &&
-sed        -i 's/"threads": [0-9]*,//g' ~/.ccminer/ccminer-gpu.conf
+sed        -i 's/"threads": [0-9]*,//g' ~/.ccminer/ccminer-gpu.conf   &&
 announce   'Creating mining scripts...    '                           &&
 echo       -e '#!/bin/bash\n\n/usr/src/ccminer/ccminer -c ~/.ccminer/ccminer-cpu.conf' > mine-cpu.sh                                   &&
 echo       -e '#!/bin/bash\n\n/usr/src/ccminer/ccminer -c ~/.ccminer/ccminer-gpu.conf' > mine-gpu.sh                                   &&
+echo       -e '#!/bin/bash\n\nscreen -dmS cpu bash -c "/usr/local/bin/mine-cpu"\nscreen -dms gpu bash -c "/usr/local/bin/mine-gpu"' > mine.sh                        &&
 announce   'Changing script permissions...'                           &&
 sudo       chmod +x mine-cpu.sh                                       &&
 sudo       chmod +x mine-gpu.sh                                       &&
+sudo       chmod +x mine.sh                                           &&
 announce   'Installing scripts...         '                           &&
 sudo       cp mine-cpu.sh /usr/local/bin/mine-cpu                     &&
 sudo       cp mine-gpu.sh /usr/local/bin/mine-gpu                     &&
+sudo       cp mine.sh /usr/local/bin/mine                             &&
 announce   'Returning to HOME...          '                           &&
 cd         ~                                                          &&
 echo       -e 'Miner installed. To begin, type "mine-cpu" or "mine-gpu"'
